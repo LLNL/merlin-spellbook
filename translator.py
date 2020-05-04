7a5a36d1866b1a9b95c29e2b19cd5d41b4723e78
Luc Peterson <peterson76@llnl.gov>
2020-05-04 16:48:00 -0700

    Add conduit based python version of collector, updated translator to take in list of subpaths

--- translator.py
+++ translator.py
@@ -31,35 +31,48 @@
 
 import argparse
 import sys
-import os
-
 import numpy as np
 
 import conduit
+
 import conduit_bundler as cb
 
 
 def setup_argparse():
     parser = argparse.ArgumentParser(
-        description='Translate a flat sample file into another format (conduit-compatible or numpy)", filtering with an external schema.')
+        description='Flatten sample file into another format (conduit-compatible or numpy)", filtering with an external schema.'
+    )
 
-    parser.add_argument('-input', help='.hdf5 file with data in it',
-                    default='results_features.hdf5')
-    parser.add_argument('-output', help='.npz file with the arrays',
-                    default='results_features.npz')
     parser.add_argument(
-    '-schema',
-    help='schema for a single sample that says what data to translate',
-    default='features_translate.json')
+        "-input", help=".hdf5 file with data in it", default="results_features.hdf5"
+    )
+    parser.add_argument(
+        "-output", help=".npz file with the arrays", default="results_features.npz"
+    )
+    parser.add_argument(
+        "-schema",
+        help="schema for a single sample that says what data to translate. Defaults to whole first node. Can be a comma-delimited list of subpaths, eg inputs,outputs/scalars,metadata",
+        default="auto",
+    )
     return parser
 
 
 def process_args(args):
     data = cb.load_node(args.input)
-    with open(args.schema, 'r') as f:
-        schema_json = f.read()
+    if args.schema == "auto":
+        schema_json = data[data.child_names()[0]].to_json()
+    elif "," in args.schema:
+        sub_list = args.schema.split(",")
+        schema_node = conduit.Node()
+        for item in sub_list:
+            schema_node[item] = data[data.child_names()[0] + "/" + item]
+        schema_json = schema_node.to_json()
+        print(schema_json)
+    else:
+        with open(args.schema, "r") as f:
+            schema_json = f.read()
 
-    g = conduit.Generator(schema_json, 'json')
+    g = conduit.Generator(schema_json, "json")
     schema = conduit.Node()
     g.walk_external(schema)
 
@@ -75,17 +88,17 @@ def process_args(args):
         filtered_node.update_compatible(unfiltered_node)
         make_data_array_dict(all_dict, filtered_node)
 
-
     # Save according to output extension, either numpy or conduit-compatible
     protocol = cb.determine_protocol(args.output)
-    if protocol == 'npz':
+    if protocol == "npz":
         np.savez(args.output, **all_dict)
     else:
-        n = conduit.Node()
-        g.walk_external(n)
-        n.update_compatible(data)
-        for data_name in all_dict.keys():
-            n[data_name] = np.array(all_dict[data_name])
+        n = cb.pack_conduit_node_from_dict(all_dict)
+        # n = conduit.Node()
+        # g.walk_external(n)
+        # n.update_compatible(data)
+        # for data_name in all_dict.keys():
+        #    n[data_name] = np.array(all_dict[data_name]).
         cb.dump_node(n, args.output)
 
 
@@ -95,10 +108,11 @@ def generate_scalar_path_pairs(node, path=""):
     for child in children:
         if isinstance(node[child], conduit.Node):
             for pair in generate_scalar_path_pairs(
-                    node[child], path=os.path.join(os.path.join(path, child), ''):
+                node[child], path=path + child + "/"
+            ):
                 yield pair
         else:
-            yield os.path.join(path, child), node[child]
+            yield path + child, node[child]
 
 
 def make_data_array_dict(d, node):
@@ -112,11 +126,9 @@ def make_data_array_dict(d, node):
 
 def main():
     parser = setup_argparse()
-    args = parser.parse_args()   
+    args = parser.parse_args()
     process_args(args)
 
- 
+
 if __name__ == "__main__":
     sys.exit(main())
-
-
