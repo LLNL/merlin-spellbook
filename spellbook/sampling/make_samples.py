32c383bcea92436ddac7409227eacf03ef09997d
Benjamin Bay <bay1@llnl.gov>
2021-02-02 12:03:15 -0700

    added central composite ccf face sampling

--- spellbook/sampling/make_samples.py
+++ spellbook/sampling/make_samples.py
@@ -9,7 +9,7 @@ from argparse import (
 )
 
 import numpy as np
-import pyDOE as doe
+import pyDOE2 as doe
 from scipy.stats.distributions import norm
 
 from spellbook.utils import prep_argparse
@@ -113,6 +113,9 @@ def process_args(args):
     elif sample_type == "star":
         _x = doe.doe_star.star(n_dims)[0]
         x = 0.5 * (_x + 1.0)  # transform to center at 0.5 (range 0-1)
+    elif sample_type == "central_composite":
+        _x = np.unique(doe.ccdesign(n_dims,face='ccf'),axis=0)
+        x = 0.5 * (_x + 1.0)
     else:
         raise ValueError(sample_type + " is not a valid choice for sample_type!")
 
@@ -195,7 +198,7 @@ def setup_argparse(parent_parser=None, the_subparser=None):
     make_samples.add_argument("-dims", help="number of dimensions", default=2, type=int)
     make_samples.add_argument(
         "-sample_type",
-        help="type of sampling. options: random, grid, lhs, lhd, star. If grid, will try to get close to the correct number of samples. for lhd min-max correspond to +- 3 sigma range",
+        help="type of sampling. options: random, grid, lhs, lhd, star, central_composite. If grid, will try to get close to the correct number of samples. for lhd min-max correspond to +- 3 sigma range",
         default="random",
     )
     make_samples.add_argument(
