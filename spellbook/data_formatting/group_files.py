a2202472e1db1dc95413684e69212be7c4515713
Benjamin Bay <bay1@llnl.gov>
2021-05-26 14:35:51 -0700

    removed extraneous code

--- spellbook/data_formatting/group_files.py
+++ spellbook/data_formatting/group_files.py
@@ -1,6 +1,4 @@
-import argparse
 import pickle as pkl
-import sys
 
 
 def get_chunks(itr, n):
@@ -17,23 +15,3 @@ def process_args(args):
     )  # TODO accomodate odd # of files, non-integer ratios
     filepath_chunks = list(get_chunks(filepaths, n_files_per_chunk))
     pkl.dump(filepath_chunks, open(args.outfile, "wb"))
-
-
-def setup_argparse():
-    parser = argparse.ArgumentParser(
-        description="Group a list of filepaths into chunks of size 'n_chunks'"
-    )
-    parser.add_argument("--n_chunks", action="store", type=int, help="Number of chunks")
-    parser.add_argument("--filepaths", nargs="+", default=[])
-    parser.set_defaults(func=process_args)
-    return parser
-
-
-def main():
-    parser = setup_argparse()
-    args = parser.parse_args()
-    return args.func(args)
-
-
-if __name__ == "__main__":
-    sys.exit(main())
