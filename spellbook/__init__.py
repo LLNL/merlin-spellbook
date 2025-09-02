6a002928d0618d8f49e821b0e44b2c1d1fcd72a0
Brian Gunnarson <brianfunnarson14@gmail.com>
2025-09-02 08:32:42 -0700

    add new copyright header to all files

--- spellbook/__init__.py
+++ spellbook/__init__.py
@@ -1,3 +1,10 @@
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other
+# Merlin-Spellbook Project developers. See top-level LICENSE and COPYRIGHT
+# files for dates and other details. No copyright assignment is required to
+# contribute to Merlin-Spellbook.
+##############################################################################
+
 __version__ = "0.9.0"
 
 VERSION = __version__
--- spellbook/optimization/qoi.py
+++ spellbook/optimization/qoi.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other
+# Merlin-Spellbook Project developers. See top-level LICENSE and COPYRIGHT
+# files for dates and other details. No copyright assignment is required to
+# contribute to Merlin-Spellbook.
+##############################################################################
+
 import numpy as np
 
 from spellbook.utils import load_infile
